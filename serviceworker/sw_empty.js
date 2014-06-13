509824d5c2a10e7b31cd2c3c9970b18b4c6bd648
Michael van Ouwerkerk <mvanouwerkerk@gmail.com>
2014-06-13 18:04:18 +0100

    Use two sw to hopefully get two registration ids.

---
+++ serviceworker/sw_empty.js
@@ -0,0 +1,12 @@
+function handler(event) {
+  console.log(event.type);
+  if (event.type == 'push') {
+    console.log(event.data);
+  }
+}
+
+['activate', 'install', 'push'].forEach(function(type) {
+  self.addEventListener(type, handler);
+});
+
+console.log('Logged from inside empty SW');
