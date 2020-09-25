fb1d079e680a86b9b8026520af76ff33b09c41a0
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2020-09-25 13:42:03 +0100

    Move app.js to common.js and use defer.

--- common.js
+++ common.js
@@ -2,7 +2,7 @@
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
+    window.addEventListener('DOMContentLoaded', function() {
       navigator.serviceWorker.register('sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
