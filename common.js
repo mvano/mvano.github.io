149264029d7c1068f0ccd3e962ec58d8b3f0d199
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2020-09-25 16:46:39 +0100

    Log userAgent.

--- common.js
+++ common.js
@@ -14,14 +14,13 @@
       }
     });
 
+    console.log(navigator.userAgent);
     console.log(window.location.href);
 
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('sw.js').then(function(registration) {
-        // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
-        // registration failed :(
         console.log('ServiceWorker registration failed: ', err);
       });
     }
